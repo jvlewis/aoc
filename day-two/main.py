bd691be37c143a5f249e71530b86915cd5ef8665
James <amesewis@protonmail.com>
2020-12-02 21:09:00 -0500

    Day Two

--- day-two/main.py
+++ day-two/main.py
@@ -4,26 +4,25 @@
 def retrieveInput(file):
   return open(file, "r").read().splitlines()
 
-# solution one - first 
-# def processPasswords(rulesAndWords):
-#     valid = 0
 
-#     for x in rulesAndWords:
-#         x = x.split(':')
-#         rule = x[0].split(" ")
-#         nums = rule[0].split("-")
-#         low = int(nums[0])
-#         high = int(nums[1])
-#         ch = rule[1]
-#         word = x[1].lstrip()
-#         count = word.count(ch)
-#         if count >= low and count <= high:
-#             valid += 1 
-
-#     return valid
-
-# solution two - first
 def processPasswords(rulesAndWords):
+    valid = 0
+
+    for x in rulesAndWords:
+        x = x.split(':')
+        rule = x[0].split(" ")
+        nums = rule[0].split("-")
+        low = int(nums[0])
+        high = int(nums[1])
+        ch = rule[1]
+        word = x[1].lstrip()
+        count = word.count(ch)
+        if count >= low and count <= high:
+            valid += 1 
+
+    return valid
+
+def processPasswordsTwo(rulesAndWords):
     valid = 0
 
     for x in rulesAndWords:
@@ -37,8 +36,6 @@ def processPasswords(rulesAndWords):
         
         try:
             if ((word[low-1] == ch) ^ (word[high-1] == ch)):
-                print(word + " - char -> " + ch)
-                print(nums)
                 valid += 1
         except:
             continue
@@ -46,4 +43,4 @@ def processPasswords(rulesAndWords):
     return valid
 
 
-print(processPasswords(retrieveInput("input.txt")))
+print(processPasswordsTwo(retrieveInput("input.txt")))
